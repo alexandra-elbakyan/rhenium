83999f98372a71579961f54804fbe9e7634be044
Alexandra Elbakyan <alexandra@dns.cymru>
2024-04-22 08:24:05 +0800

    search embedding vector in redis

--- app.jl
+++ app.jl
@@ -4,6 +4,7 @@ using GenieFramework
 using Genie.Renderer.Html, Genie.Requests
 using WordTokenizers, Statistics
 using LibPQ, Tables, Jedis
+using PyCall
 
 redis = Client(host = "localhost", port = 6379)
 datac = LibPQ.Connection("dbname=science host=localhost user=researchers password=KRASLApQ6QjE6hX6ff")
@@ -35,6 +36,29 @@ function getvector(title, text)
 end
 
 
+py"""
+
+import redis
+import numpy
+from redis.commands.search.query import Query
+
+def runquery(vector):
+    query = (
+        Query("(*)=>[KNN 10000 @embedding $vector AS score]")
+        .sort_by("score")
+        .paging(8000, 32)
+        .return_fields("score", "i")
+        .dialect(2)
+    )
+    bytes = numpy.array(vector, dtype = numpy.float32).tobytes()
+    client = redis.Redis(port = 6379, decode_responses = True)
+    result = client.ft("word2vec").search(query, {"vector": bytes})
+    return result.docs
+
+"""
+
+
+
 route("/") do
    html(path"app.jl.html", results = "", query = "")
 end
@@ -43,7 +67,10 @@ route("/", method = POST) do
    query = postpayload(:query)
    qvector = vec(getvector("", query))
    embedding = Pgvector.convert(qvector)
-   result = LibPQ.execute(datac, "SELECT id, title, year, abstract FROM arxiv ORDER BY embedding <=> \$1 LIMIT 32", [embedding])
+   results = py"runquery"(embedding)
+   ids = join([p.i for p in results], ", ")
+   # result = LibPQ.execute(datac, "SELECT id, title, year, abstract FROM arxiv ORDER BY embedding <=> \$1 LIMIT 32", [embedding])
+   result = LibPQ.execute(datac, "SELECT id, title, year, abstract FROM arxiv WHERE i IN (" * ids * ")")
    results = rowtable(result)
    html(path"app.jl.html", query = query, results = results)
 end
