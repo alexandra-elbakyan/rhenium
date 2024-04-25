ac4f073af8f4abdae4b1a49a6f9c634c3cc0092a
Alexandra Elbakyan <alexandra@dns.cymru>
2024-04-25 10:36:17 +0700

    lot of updates e.g. infinite scroll

--- app.jl
+++ app.jl
@@ -1,39 +1,42 @@
 module Science
 
 using GenieFramework
-using Genie.Renderer.Html, Genie.Requests
+using Genie.Renderer.Html
+using Genie.Requests
 using WordTokenizers, Statistics
 using LibPQ, Tables, Jedis
+using DataFrames
 using PyCall
+
+import Genie.Renderer.Json: json
 
 redis = Client(host = "localhost", port = 6379)
 datac = LibPQ.Connection("dbname=science host=localhost user=researchers password=KRASLApQ6QjE6hX6ff")
 
 println("connected to databases.")
 
-#module Pgvector
-#    convert(v::AbstractVector{T}) where T<:Real = string("[", join(v, ","), "]")
-#end
 
-function wordvector(word, N)
-    global redis
+function word2vec(query)
 
-    vector = Jedis.hget("embeddings", word; client = redis)
-    if isnothing(vector)
-        vector = Jedis.hget("embeddings", lowercase(word); client = redis)
+    function wordvec(word, N)
+        global redis
+        vector = Jedis.hget("vectors:word2vec", word; client = redis)
+        if isnothing(vector)
+            vector = Jedis.hget("vectors:word2vec", lowercase(word); client = redis)
+        end
+        if isnothing(vector)
+            Float32.(vec(zeros(1, N)))
+        else
+            parse.(Float32,split(chop(vector,head=1),","))
+        end
     end
-    if isnothing(vector)
-        Float32.(vec(zeros(1, N)))
-    else
-        parse.(Float32,split(chop(vector,head=1),","))
-    end
+
+    words = reduce(vcat, nltk_word_tokenize.(split_sentences(query)))
+    vectors = stack(wordvec.(words, 300))
+    vec(mean(vectors, dims = 2))
+
 end
 
-function getvector(title, text)
-    words = reduce(vcat, nltk_word_tokenize.(split_sentences(title * "\n" * text)))
-    vectors = stack(wordvector.(words, 300))
-    mean(vectors, dims = 2)
-end
 
 
 py"""
@@ -42,48 +45,53 @@ import redis
 import numpy
 from redis.commands.search.query import Query
 
-def runquery(vector):
+def runquery(vector, model, start, N):
     query = (
-        Query("(*)=>[KNN 10000 @embedding $vector AS score]")
+        Query("(*)=>[KNN 8192 @" + model + " $vector AS score]")
         .sort_by("score")
-        .paging(0, 32)
+        .paging(start, N)
         .return_fields("score", "i")
         .dialect(2)
     )
     bytes = numpy.array(vector, dtype = numpy.float32).tobytes()
     client = redis.Redis(port = 6379, decode_responses = True)
-    result = client.ft("word2vec").search(query, {"vector": bytes})
+    result = client.ft(model).search(query, {"vector": bytes})
     return result.docs
 
 """
 
+function search(query, model, start = 0, N = 32)
+    global datac
+    if model in ["word2vec", "gte-large-en-v1.5"]
+        vector = getfield(Science, Symbol(model))(query)
+        results = py"runquery"(vector, model, start, N)
+        records = DataFrame([NamedTuple([:i => parse(Int, r.i), :score => parse(Float32, r.score)])  for r in results])
+        result = LibPQ.execute(datac, "SELECT i, id, title, year, abstract FROM arxiv WHERE i IN (" * join([p.i for p in results], ", ") * ")")
+        results = DataFrame(columntable(result))
+        results = leftjoin(results, records, on = [:i])
+        [NamedTuple(result) for result in eachrow(results)]
+    end
+end
 
 
-route("/") do
+route("/", method = GET) do
    html(path"app.jl.html", results = "", query = "")
 end
 
-route("/", method = POST) do
-   query = postpayload(:query)
-   qvector = vec(getvector("", query))
-#   embedding = Pgvector.convert(qvector)
-   results = py"runquery"(qvector)
-   
-   ids = join([p.i for p in results], ", ")
-   scores = Dict(parse(Int, p.i) => parse(Float32, p.score) for p in results)
-   scores = sort(collect(scores), by = i -> i[2])
-
-   # result = LibPQ.execute(datac, "SELECT id, title, year, abstract FROM arxiv ORDER BY embedding <=> \$1 LIMIT 32", [embedding])
-   result = LibPQ.execute(datac, "SELECT i, id, title, year, abstract FROM arxiv WHERE i IN (" * ids * ")")
-   results = rowtable(result)
-
-   metadata = Dict([p.i => (p.id, p.title, p.year, p.abstract) for p in results])
-   metadata = [[score, metadata[i]...] for (i, score) in scores]
-
-   html(path"app.jl.html", query = query, results = metadata)
+route("/:model/:query", method = GET) do
+   model = payload(:model)
+   query = payload(:query)
+   elapsed = @elapsed results = search(query, model)
+   html(path"app.jl.html", query = query, results = results, model = model, count = length(results), time = elapsed)
 end
 
+route("/scroll/:model/:query/:start", method = GET) do
+    model = payload(:model)
+    query = payload(:query)
+    start = payload(:start)
+    search(query, model, start) |> json
+end
 
-Server.up(8000)
+Server.up(1234)
 
 end
